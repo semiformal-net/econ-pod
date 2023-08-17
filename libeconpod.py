b06b53f3425b8ee138577f5ef9e6574616743cc6
Paul Edwards <paul.edwards@semiformal.net>
2023-08-17 18:46:25 -0400

    no sql; uses a pickle file in data folder to store state

---
+++ libeconpod.py
@@ -0,0 +1,283 @@
+from flask import Flask, render_template, make_response, send_from_directory, send_file, url_for
+import json
+import os
+import sys
+import datetime
+import sqlite3
+import pandas as pd
+# for zip file download and extract
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from zipfile import ZipFile
+from io import BytesIO
+import pickle
+#from apscheduler.schedulers.background import BackgroundScheduler
+from flask_apscheduler import APScheduler
+import atexit
+import requests
+
+###############################################################
+#
+# CONSTANTS
+#
+###############################################################
+
+PICKLE_PATH='/data/current_issue.pkl'
+baseUrl = os.getenv('BASE_URL')
+if baseUrl is None:
+    baseUrl='http://127.0.0.1:5500/'
+gotify_host = os.getenv('GOTIFY_HOST')
+if gotify_host is None:
+    gotify_host='http://127.0.0.1:8008'
+gotify_host=gotify_host.rstrip('/') # no trailing slash
+gotify_token = os.getenv('GOTIFY_TOKEN')
+if gotify_token is None:
+    gotify_token='SeCrEt'
+
+###############################################################
+#
+# CLASSES
+#
+###############################################################
+
+# set configuration values
+class Config:
+    SCHEDULER_API_ENABLED = True
+
+class Podcast:
+    def __init__(self, publication_date=None, is_published: bool=False, issue_number: int=0):
+        self.url = self.date_issue_to_url(publication_date,issue_number)
+        self.publication_date = publication_date
+        self.is_published = is_published
+        self.issue_number = issue_number
+
+    def date_issue_to_url(self,date,issue):
+        year = date.strftime('%Y')
+        month = date.strftime('%m')
+        day = date.strftime('%d')
+        date = date.strftime('%Y%m%d')
+        issuezip="http://audiocdn.economist.com/sites/default/files/AudioArchive/{0}/{2}/Issue_{1}_{2}_The_Economist_Full_edition.zip".format(year, issue, date)
+        return issuezip
+
+    def issue_ready(self):
+        if self.url is None:
+            self.is_published = False
+            return False
+        try:
+            a=urlopen(self.url)
+        except HTTPError as e:
+            # "e" can be treated as a http.client.HTTPResponse object
+            #print('Error: fetching {}: {}'.format(issuezip,e))
+            self.is_published=False
+        else:
+            self.is_published=True
+
+        return self.is_published
+
+base_podcasts={
+"baseUrl" : baseUrl,
+"podcasts" : {
+    "podcast1" : {
+    "title": "podcast1",
+    "author" : "podcast1",
+    "contactEmail" : "podcast@podcast.com",
+    "contactName" : "Podcaster name",
+    "description" : "podcast1 description",
+    "languaje" : "en-us",
+    "coverFilename" : "podcast1/economist_logo.png",
+    "rssUrl" : "podcast1/rss",
+    "audiosFolder" : "podcast1/audios/"
+    }
+    }
+}
+
+###############################################################
+#
+# FUNCTIONS
+#
+###############################################################
+
+def get_current_issue_from_db():
+    #
+    # The rule is that the DB shall always store the last available (is_published=True) issue
+    #
+
+    if not os.path.isfile(PICKLE_PATH):
+        return None
+
+    if not os.access(PICKLE_PATH, os.R_OK):
+        return None
+
+    with open(PICKLE_PATH, 'rb') as f:
+        current_issue=pickle.load(f)
+    return current_issue
+
+def put_current_issue_to_db(current_issue):
+    #
+    # The rule is that the DB shall always store the last available (is_published=True) issue
+    #
+
+    with open(PICKLE_PATH, 'wb') as f:
+        pickle.dump(current_issue, f)
+
+def same_week_as_xmas(date):
+    # Get the week number of Christmas for the given year
+    xmas_year = date.year
+    xmas_week_num = datetime.date(xmas_year, 12, 25).isocalendar()[1]
+
+    # Get the week number of the given date
+    input_week_num = date.isocalendar()[1]
+
+    # Check if the week numbers are the same
+    return input_week_num == xmas_week_num
+
+def next_issue(current_issue):
+    if not isinstance(current_issue, Podcast):
+        return None
+    saturdays=pd.date_range(current_issue.publication_date , current_issue.publication_date + datetime.timedelta(21) ,freq='W-SAT') # a list of saturdays, starting with current issue
+
+    now=datetime.datetime.now()
+
+    i=1
+    # if the saturday occurs on the week of xmas, or if the saturday occurs in the past, then skip
+    # Econ skips an issue in the summer
+    # so in effect this will seek through saturdays looking for an issue
+    while (same_week_as_xmas(saturdays[i]) or (now-saturdays[i]).days > 0 ):
+        i=i+1
+    next_issue = Podcast(publication_date=saturdays[i], is_published=False, issue_number=current_issue.issue_number+1)
+    return next_issue
+
+def init_current_issue():
+
+    current_issue=get_current_issue_from_db()
+    if current_issue is None:
+        # This is the cold start logic. If you are unable to warm start with a valid issue.
+        schedule_day=build_schedule()
+        issues=build_issues(schedule_day)
+        valid_day, issuezip=find_valid_issue(schedule_day,issues) # the zip file to grab
+        current_issue = Podcast(publication_date=valid_day[0], is_published=True, issue_number=valid_day[1])
+        print('[*] Warning: Cold start, detected issue {} ({})'.format(current_issue.publication_date,current_issue.issue_number))
+    else:
+        print('Found issue {} ({}) in database, resuming'.format(current_issue.publication_date,current_issue.issue_number))
+
+    return current_issue
+
+def dl_issue(issuezip):
+
+    #
+    # assumes that url has been checked
+    #
+
+    now = datetime.datetime.now()
+    print('[*] Fetching {}'.format(issuezip))
+
+    # unzip on the fly
+    with urlopen(issuezip) as zipresp:
+        with ZipFile(BytesIO(zipresp.read())) as zfile:
+            zfile.extractall('/app/static/podcast1/audios') # put unzipped files into the podcast static dir
+
+    now2 = datetime.datetime.now()
+    dltime=(now2-now).total_seconds()
+
+    return dltime
+
+def build_schedule():
+    #
+    # Build schedule_day[] and issues[] lists: code courtesy of https://github.com/evmn/the-economist
+    #
+    # next_saturday
+    d = datetime.date.today()
+    t = datetime.timedelta((12 - d.weekday()) % 7)
+    if t.days == 0: # if today is a saturday jump ahead a week
+        t=datetime.timedelta(days=7)
+    next_saturday=d + t
+    schedule_day = pd.date_range('20120101',next_saturday.strftime('%Y%m%d'),freq='W-SAT') # a long list of saturdays
+    return schedule_day
+
+def build_issues(schedule_day):
+    # issue number can becomputed from a date.
+    # build a list of issues corresonding to each date in the schedules
+    issues=[]
+    weeks=0
+    for i in schedule_day:
+        year = i.strftime('%Y')
+        month = i.strftime('%m')
+        day = i.strftime('%d')
+        date = i.strftime('%Y%m%d')
+        issue = 8766+weeks
+        issue = issue - 2 # issue got out of sync
+        issues.append(issue)
+        if (int(month) != 12) or (int(day) < 25): # no issue near xmas
+            weeks=weeks+1
+    return issues
+
+def issue_ready(date,issue):
+    issue_ready=False
+    year = date.strftime('%Y')
+    month = date.strftime('%m')
+    day = date.strftime('%d')
+    date = date.strftime('%Y%m%d')
+
+    issuezip="http://audiocdn.economist.com/sites/default/files/AudioArchive/{0}/{2}/Issue_{1}_{2}_The_Economist_Full_edition.zip".format(year, issue, date)
+    try:
+        a=urlopen(issuezip)
+    except HTTPError as e:
+        # "e" can be treated as a http.client.HTTPResponse object
+        pass #print('Error: fetching {}: {}'.format(issuezip,e))
+    else:
+        issue_ready=True
+
+    return issue_ready, issuezip
+
+def find_valid_issue(schedule_day,issues):
+    for i in list(zip( schedule_day[:-4:-1], issues[:-4:-1])): # last three (!) items in list, in reverse order
+        ready, issuezip=issue_ready(i[0],i[1])
+        if ready:
+            return i,issuezip
+
+    if not ready:
+        print("Error: Unable to get an issue")
+    sys.exit(4)
+
+def build_json(base_json):
+    podcasts = base_json # copy
+    audios=[]
+    adir='static/'+podcasts['podcasts']['podcast1']['audiosFolder']
+    counter=0
+    sizecounter=0
+    # iterate over files in that directory
+    for filename in sorted(os.listdir( adir )):
+        f = os.path.join(adir, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            fname=os.path.basename(f)
+            pname, ext = os.path.splitext(fname)
+            # only process mp3s
+            if not ext.lower() == '.mp3':
+                continue
+            F={"title": pname,
+            "description": pname,
+            "filename":fname,
+            "date": datetime.datetime.fromtimestamp( os.path.getmtime(f) )
+                .strftime( "%a, %d %b %Y %H:%M:%S -05:00"),
+            "length": os.path.getsize(f)
+            }
+            audios.append(F)
+            counter=counter+1
+            sizecounter=sizecounter+os.path.getsize(f)
+
+    podcasts['podcasts']['podcast1']['audios'] = audios
+    return counter, sizecounter, podcasts
+
+def gotify_push(msg):
+    try:
+        resp = requests.post('{}/message?token={}'.format(gotify_host,gotify_token), json={
+        "message": msg, # completely uncechked input
+        "priority": 2,
+        "title": "Econpod"})
+    except requests.exceptions.RequestException as e:
+        print('Failed to push notification to {}'.format(gotify_host))
+        print(e)
+#
+# END FUNCTIONS ---------------------------------------------------------------------------
+#
